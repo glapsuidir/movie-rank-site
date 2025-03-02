54160b7b6b5556aa887220c2d42ffd5507f63173
Glapsuidir <jamiejtoone@gmail.com>
2025-03-01 18:35:46 -0800

    Modified the API key to use https instead of http

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ class MovieManager {
     }
 
     async searchMovie(title) {
-        const response = await fetch(`http://www.omdbapi.com/?t=${encodeURIComponent(title)}&apikey=${this.OMDB_API_KEY}`);
+        const response = await fetch(`https://www.omdbapi.com/?t=${encodeURIComponent(title)}&apikey=${this.OMDB_API_KEY}`);
         const data = await response.json();
         return data;
     }
